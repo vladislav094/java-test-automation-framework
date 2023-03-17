8301be91740a025cefb66362f03d6507ef4bc3cd
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-17 22:21:15 +0300

    added new branch for garbage and added headless mode in ChromeOptions

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -1,6 +1,7 @@
 package core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,7 +10,7 @@ import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
-//import static readProperties.ConfigurationManager.configuration;
+import static readProperties.ConfigurationManager.configuration;
 import static readProperties.ConfigWebApplication.IsHeadless;
 import static readProperties.ConfigWebApplication.readConfig;
 
@@ -19,8 +20,9 @@ abstract public class BaseSeleniumTest {
     @BeforeTest
     public void setUp(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(getOptions());
 //        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         BaseSeleniumPage.setDriver(driver);
@@ -29,7 +31,7 @@ abstract public class BaseSeleniumTest {
     private ChromeOptions getOptions() {
         ChromeOptions options = new ChromeOptions();
         options.setAcceptInsecureCerts(true);
-//        options.setHeadless(readConfig().getBoolean(IsHeadless));;
+        options.setHeadless(configuration().headless());;
         return options;
     }
 
